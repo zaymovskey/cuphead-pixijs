3951c90bc49b0eee07f611e4dc65d3b0a93ca32e
zaymovskey <zaymovskey@mail.com>
2024-08-11 15:18:51 +0300

    Цвет платформ

--- src/entities/platforms/PlatformView.ts
+++ src/entities/platforms/PlatformView.ts
@@ -1,14 +1,19 @@
+import { DEFAULT_STROKE_WIDTH } from '@/consts/global.ts';
 import { BaseView } from '@/entities/BaseView.ts';
 import { IPlatformSize } from '@/entities/platforms/Platform.ts';
 import { Graphics } from 'pixi.js';
 
 export class PlatformView extends BaseView {
+  platformColor: string = '#0884bc';
+
   constructor(size: IPlatformSize) {
     super();
 
     const platform = new Graphics()
       .rect(this.x, this.y, size.width, size.height)
-      .stroke('#ecec19');
+      .stroke(this.platformColor);
+
+    platform.strokeStyle.width = DEFAULT_STROKE_WIDTH;
 
     this.addChild(platform);
   }
